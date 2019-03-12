260cdc444a71988f87a33bcb50b1665b716e875c
GenBor <Generalovboris@outlook.com>
2019-03-12 14:00:30 +0200

    create endpoints

--- my_blog/urls.py
+++ my_blog/urls.py
@@ -15,7 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import url, include
+from my_blog.views import article as views
+from my_blog.views.article import PostsListView, PostDetailView
+from django.conf.urls.static import static
+from my_blog import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+    url('post-list/', PostsListView.as_view(), name='list'),
+    url(r'^(?P<pk>\d+)/$', PostDetailView.as_view()),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
