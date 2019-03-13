9eca2ea281b9e687637c4daf13b159a6b4411015
GenBor <Generalovboris@outlook.com>
2019-03-13 14:52:52 +0200

    views

--- my_blog/views/article.py
+++ my_blog/views/article.py
@@ -1,9 +1,93 @@
-from my_blog.models.article import Article
-from django.views.generic import ListView, DetailView
+from django.contrib.auth.models import User
+from django.http import JsonResponse, Http404
+from django.shortcuts import redirect, get_object_or_404
+from django.urls import reverse
+from django.utils.decorators import method_decorator
+from django.views.generic import DetailView, CreateView, View, TemplateView, DeleteView
+
+from my_blog.decorators import authorized_only
+from my_blog.forms import ArticleForm
+from my_blog.models import Article, Subscription, ArticleRead
 
 
-class PostsListView(ListView): # представление в виде списка
-    model = Article                  # модель для представления 
+@method_decorator(authorized_only, name='dispatch')
+class SubscribeView(View):
 
-class PostDetailView(DetailView): # детализированное представление модели
+    def post(self, request):
+        try:
+            user_id = request.POST.get('pk')
+            user = User.objects.get(pk=user_id)
+        except User.DoesNotExist:
+            return JsonResponse({'status': 'error', 'error': 'User not found'})
+
+        sub, created = Subscription.objects.get_or_create(user=user)
+
+        if request.user in sub.subscribers.all():
+            sub.subscribers.remove(request.user.id)
+            subscribed = False
+            Article.objects.filter(user_id=request.user.id, post__user_id=user_id).delete()
+        else:
+            sub.subscribers.add(request.user.id)
+            subscribed = True
+        sub.save()
+
+        return JsonResponse({'status': 'ok', 'subscribed': subscribed})
+
+
+class BlogView(DetailView):
+    model = User
+    template_name = 'blog/user.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(BlogView, self).get_context_data(**kwargs)
+        context['posts'] = Article.objects.filter(user_id=self.object.id).prefetch_related('user')
+        return context
+
+
+class PostCreateView(CreateView):
     model = Article
+    form_class = ArticleForm
+    template_name = 'blog/create.html'
+
+    def form_valid(self, form):
+        post = form.save(commit=False)
+        post.user = self.request.user
+        post.save()
+        return redirect(reverse('blog:detail', args=(post.user_id, post.id)))
+
+
+class PostDetailView(DetailView):
+    model = Article
+    template_name = 'blog/detail.html'
+
+
+class HomeView(TemplateView):
+    template_name = 'home.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(HomeView, self).get_context_data(**kwargs)
+        subscription = list(self.request.user.subscription.values_list('user_id', flat=True))
+        subscription.append(self.request.user.id)
+        context['posts'] = Article.objects.filter(user_id__in=subscription).prefetch_related('user').distinct()
+        return context
+
+
+class PostReadView(View):
+    def post(self, request):
+        try:
+            post = Article.objects.exclude(user_id=request.user.id).get(pk=request.POST.get('post_id'))
+        except Article.DoesNotExist:
+            return JsonResponse({'status': 'error', 'error': 'Пост не найден'})
+
+        ArticleRead.objects.get_or_create(user=request.user, post=post)
+        return JsonResponse({'status': 'ok'})
+
+
+class PostDeleteView(DeleteView):
+    model = Article
+
+    def get_object(self, queryset=None):
+        return get_object_or_404(Article, user_id=self.request.user.id, pk=self.kwargs.get('pk'))
+
+    def get_success_url(self):
+        return reverse('blog:home')
