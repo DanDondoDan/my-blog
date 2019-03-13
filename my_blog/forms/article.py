bbfb76447a76d42a83719a7c41941139f2d0328c
GenBor <Generalovboris@outlook.com>
2019-03-13 15:17:42 +0200

    fix debug

--- my_blog/forms/article.py
+++ my_blog/forms/article.py
@@ -6,4 +6,4 @@ from my_blog.models import Article
 class ArticleForm(forms.ModelForm):
     class Meta:
         model = Article
-        fields = ('header', 'context')
+        fields = ('header', 'content')
